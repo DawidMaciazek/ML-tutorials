7b93dec6f660b8e76c02e73bb375488a390bbee2
Dawid Maciazek <dawid.maciazek@gmail.com>
2017-02-22 20:27:27 +0100

    update

--- tensor/mnist_0/mnist0.py
+++ tensor/mnist_0/mnist0.py
@@ -1,35 +1,60 @@
-import sys
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-
-#sys.exit(0)
 import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
 
-x = tf.placeholder(tf.float32, [None, 784])
 
-W = tf.Variable(tf.zeros([784, 10]))
-W1 = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros([10]))
+class MinstTester:
 
-y = tf.nn.softmax(tf.matmul(x, W) + b)
-y_ = tf.placeholder(tf.float32, [None, 10])
+    def __init__(self):
+        self.mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+        self.x = tf.placeholder(tf.float32, [None, 784])
 
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+        self.W = tf.Variable(tf.zeros([784, 10]))
+        self.b = tf.Variable(tf.zeros([10]))
 
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+        self.y = tf.nn.softmax(tf.matmul(self.x, self.W) + self.b)
+        self.y_ = tf.placeholder(tf.float32, [None, 10])
 
-sess = tf.InteractiveSession()
+        self.cross_entropy = tf.reduce_mean(-tf.reduce_sum(self.y_ * tf.log(self.y), reduction_indices=[1]))
+        self.train_step = tf.train.GradientDescentOptimizer(0.5).minimize(self.cross_entropy)
+        self.sess = tf.InteractiveSession()
 
-#tf.global_variables_initializer().run()
-tf.initialize_all_variables().run()
+        tf.global_variables_initializer().run()
 
-print "lets start loop"
-for step in range(1000):
-  batch_xs, batch_ys = mnist.train.next_batch(100)
-  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
-print "lest check results"
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-print correct_prediction
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+    def train(self, steps=1000):
+
+        for step in range(steps):
+            batch_xs, batch_ys = self.mnist.train.next_batch(100)
+            self.sess.run(self.train_step, feed_dict={self.x: batch_xs, self.y_: batch_ys})
+
+
+    def check_prediction(self, look_range=None):
+        correct_prediction = tf.equal(tf.argmax(self.y,1), tf.argmax(self.y_,1))
+
+        correct_list = self.sess.run(correct_prediction, feed_dict={self.x: self.mnist.test.images, self.y_: self.mnist.test.labels})
+
+        if look_range is not None:
+            print(correct_list[look_range[0]:look_range[1]])
+            for i in range(look_range[0], look_range[1]):
+                if not correct_list[i]:
+                    self.show_image(i)
+
+        #accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        #print(self.sess.run(accuracy, feed_dict={self.x: self.mnist.test.images, self.y_: self.mnist.test.labels}))
+        #print(self.sess.run(accuracy, feed_dict={self.x: self.mnist.test.images, self.y_: self.mnist.test.labels}))
+
+    def show_image(self, image_index):
+        pixels = self.mnist.test.images[image_index].reshape((28, 28))
+
+        label = self.mnist.test.labels[image_index]
+        plt.title('Label is {label}'.format(label=label))
+        plt.imshow(pixels, cmap='gray')
+        plt.show()
+
+
+mtest =  MinstTester()
+mtest.train(1000)
+mtest.check_prediction([950,1000])
+
